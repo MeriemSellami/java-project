866410438d994e7d24dcd3dcdc23c5457e10fc9d
MeriemSellami <119887701+MeriemSellami@users.noreply.github.com>
2023-12-30 21:48:09 +0100

    Update acceuil.java

--- javaproject/src/acceuil.java
+++ javaproject/src/acceuil.java
@@ -31,35 +31,23 @@ public class acceuil extends JFrame {
         });
 
         p1.add(t1, gbc);
-
-        // Set button background color to light purple
-        Color lightPurple = new Color(230, 230, 250); // Adjust the RGB values for a lighter shade of purple
+        Color lightPurple = new Color(230, 230, 250);
         Color pinkColor = new Color(255,182,193);
         b1.setBackground(pinkColor);
         b2.setBackground(pinkColor);
-
-        // Set button foreground (text) color to black
         b1.setForeground(Color.BLACK);
         b2.setForeground(Color.BLACK);
-
-        // Set the size of buttons
-        Dimension buttonSize = new Dimension(100, 30); // Adjust the dimensions as needed
+        Dimension buttonSize = new Dimension(100, 30); 
         b1.setPreferredSize(buttonSize);
         b2.setPreferredSize(buttonSize);
-
         p1.add(b1, gbc);
         p1.add(b2, gbc);
-
-        // Set the background color to light purple for the panel
         p1.setBackground(lightPurple);
-
-        // Center the panel on the JFrame
         add(p1, BorderLayout.CENTER);
-
         setTitle("Account Login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack(); // Automatically adjust frame size to fit components
-        setLocationRelativeTo(null); // Center the frame on the screen
+        pack(); 
+        setLocationRelativeTo(null); 
         setVisible(true);
     }
 
