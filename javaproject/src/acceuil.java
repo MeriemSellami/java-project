69de9974b3ed3ef8c68c0acef3b45e8b89fc1540
Meriem Sellami <119887701+MeriemSellami@users.noreply.github.com>
2023-12-31 01:28:35 +0100

    Update acceuil.java

--- javaproject/src/acceuil.java
+++ javaproject/src/acceuil.java
@@ -5,20 +5,20 @@ import java.awt.event.ActionListener;
 
 public class acceuil extends JFrame {
     public acceuil() {
-        JLabel t1 = new JLabel("Create your account or login");
-        JButton b1 = new JButton("LogIn");
-        JButton b2 = new JButton("SignUp");
-        JPanel p1 = new JPanel(new GridBagLayout());
+        JLabel t1 = new JLabel("admin or new user");
+        JButton bl = new JButton("LogIn");
+        JButton bs = new JButton("SignUp");
+        JPanel p = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
-        b1.addActionListener(new ActionListener() {
+        bl.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 admin a = new admin();
                 a.setVisible(true);
             }
         });
-        b2.addActionListener(new ActionListener() {
+        bs.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 user c = new user();
@@ -28,16 +28,16 @@ public class acceuil extends JFrame {
         p1.add(t1, gbc);
         Color lightPurple = new Color(230, 230, 250);
         Color pinkColor = new Color(255,182,193);
-        b1.setBackground(pinkColor);
-        b2.setBackground(pinkColor);
-        b1.setForeground(Color.BLACK);
-        b2.setForeground(Color.BLACK);
+        bl.setBackground(pinkColor);
+        bs.setBackground(pinkColor);
+        bl.setForeground(Color.BLACK);
+        bs.setForeground(Color.BLACK);
         Dimension buttonSize = new Dimension(100, 30); 
-        b1.setPreferredSize(buttonSize);
-        b2.setPreferredSize(buttonSize);
-        p1.add(b1, gbc);
-        p1.add(b2, gbc);
-        p1.setBackground(lightPurple);
+        bl.setPreferredSize(buttonSize);
+        bs.setPreferredSize(buttonSize);
+        p.add(bl, gbc);
+        p.add(bs, gbc);
+        p.setBackground(lightPurple);
         add(p1, BorderLayout.CENTER);
         setTitle("Account Login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
